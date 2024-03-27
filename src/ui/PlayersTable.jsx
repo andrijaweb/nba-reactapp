31a0e29d668c98a28c97f09aafe0e33966272936
andrijaweb <andrijawebd@gmail.com>
2024-03-27 23:26:52 +0100

    PlayersTable styled

--- src/ui/PlayersTable.jsx
+++ src/ui/PlayersTable.jsx
@@ -1,28 +1,28 @@
 function PlayersTable() {
   return (
-    <table className="">
+    <table className="w-full border-collapse border-spacing-0 border-b">
       <thead>
-        <tr>
-          <th>Player</th>
-          <th>Team</th>
-          <th>Number</th>
-          <th>Position</th>
-          <th>Height</th>
-          <th>Weight</th>
-          <th>City</th>
-          <th>Country</th>
+        <tr className="text-left bg-blue-200 rounded-md text-sm uppercase text-stone-700">
+          <th className="p-4">Player</th>
+          <th className="p-4">Team</th>
+          <th className="p-4 text-right">Number</th>
+          <th className="p-4">Position</th>
+          <th className="p-4 text-right">Height</th>
+          <th className="p-4">Weight</th>
+          <th className="p-4">City</th>
+          <th className="p-4">Country</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Full Name</td>
-          <td>BOS</td>
-          <td>1</td>
-          <td>F</td>
-          <td>6'9</td>
-          <td>209 lbs</td>
-          <td>Subotica</td>
-          <td>Serbia</td>
+        <tr className="text-sm font-semibold text-stone-900 hover:bg-blue-50 transition-all duration-300">
+          <td className="p-4 border-b border-t text-blue-500">Full Name</td>
+          <td className="p-4 border-b border-t text-blue-500">BOS</td>
+          <td className="p-4 border-b border-t text-right">1</td>
+          <td className="p-4 border-b border-t">F</td>
+          <td className="p-4 border-b border-t text-right">6'9</td>
+          <td className="p-4 border-b border-t">209 lbs</td>
+          <td className="p-4 border-b border-t">Subotica</td>
+          <td className="p-4 border-b border-t">Serbia</td>
         </tr>
       </tbody>
     </table>
